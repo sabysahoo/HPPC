8cf928477e018a5f90f627b4f41e80ddd3c371c8
zalaniz2 <zachalaniz2@gmail.com>
2019-12-03 16:06:42 -0600

    Added more to output method.

--- output.cpp
+++ output.cpp
@@ -1,22 +1,206 @@
 #include <iostream>
 #include "header.h"
-using namespace std; 
+using namespace std;
 
-void printRoadTop(int vertical_0[], int vertical_1[], int vertical_2[], int vertical_3[],  int road_size){
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+void printRoadTop(int vertical_0[], int vertical_1[], int vertical_2[], int vertical_3[], int horizontal_0[], int horizontal_1[], int horizontal_2[], int horizontal_3[], int road_size, bool light_holder[]){
+
   int section_xy = (road_size - 4)/2;
-  for(int i= 0; i < section_xy; i++){
-    for(int j= 0; j < section_xy; j++){
-      cout<< "+" <<"\t";
-    } 
-    cout<< vertical_0[i]<<"\t"<<
-    vertical_1[i]<<"\t"<<
-    vertical_2[i]<<"\t"<<
-    vertical_3[i]<<"\t";     
-    for(int j= 0; j < section_xy; j++){
+
+  for(int i= 0; i<section_xy; i++){
+
+    cout << BOLDWHITE;
+
+    for(int j= 0; j<section_xy; j++){
       cout<< "+" <<"\t";
     }
+
+    cout << RESET;
+
+    if( vertical_0[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+    cout<< vertical_0[i]<< "\t";
+    cout << RESET;
+
+    if( vertical_1[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+
+    cout<< vertical_1[i]<< "\t";
+    cout << RESET;
+
+    if( vertical_2[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+
+    cout<< vertical_2[i]<< "\t";
+    cout << RESET;
+
+    if( vertical_3[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+
+    cout<< vertical_3[i]<< "\t";
+
+    cout << BOLDWHITE;
+
+
+    for(int j=0; j<section_xy; j++){
+      cout<< "+" <<"\t";
+    }
+
+    cout<< "\n";
+    cout<< "\n";
+
+  }
+
+  cout << RESET;
+
+  for( int i = 0; i<road_size; i++){
+    if( horizontal_0[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+      cout << horizontal_0[i] << "\t";
+  }
+
+  cout<< "\n";
+  cout<< "\n";
+
+  for( int i = 0; i<road_size; i++){
+    if( horizontal_1[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+      cout << horizontal_1[i] << "\t";
+  }
+  cout<< "\n";
+  cout<< "\n";
+
+  for( int i = 0; i<road_size; i++){
+    if( horizontal_2[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+      cout << horizontal_2[i] << "\t";
+  }
+  cout<< "\n";
+  cout<< "\n";
+
+
+  for( int i = 0; i<road_size; i++){
+    if( horizontal_3[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+      cout << horizontal_3[i] << "\t";
+  }
+  cout<< "\n";
+  cout<< "\n";
+
+  cout<< BOLDWHITE;
+
+  for(int i= section_xy+4; i<road_size; i++){
+
+    cout << BOLDWHITE;
+
+    for(int j= 0; j<section_xy; j++){
+      cout<< "+" <<"\t";
+    }
+
+    cout << RESET;
+
+    if( vertical_0[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+    cout<< vertical_0[i]<< "\t";
+    cout << RESET;
+
+    if( vertical_1[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+
+    cout<< vertical_1[i]<< "\t";
+    cout << RESET;
+
+    if( vertical_2[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+
+    cout<< vertical_2[i]<< "\t";
+    cout << RESET;
+
+    if( vertical_3[i] > 0){
+      cout << BOLDBLACK;
+    }
+    else{
+      cout << BOLDCYAN;
+    }
+
+    cout<< vertical_3[i]<< "\t";
+
+    cout << BOLDWHITE;
+
+    cout << BOLDWHITE;
+
+    for(int j=0; j<section_xy; j++){
+      cout<< "+" <<"\t";
+    }
+
+    cout << RESET;
+
+    cout<< "\n";
     cout<< "\n";
   }
+
+
 }
 
 void printRoadData(int vertical_0[], int vertical_1[], int vertical_2[], int vertical_3[], int horizontal_0[], int horizontal_1[] ,int horizontal_2[] , int horizontal_3[], int road_size){
--- traffic.cpp
+++ traffic.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include "header.h"
 #include <unistd.h>
-using namespace std; 
+using namespace std;
 
 // Cars States: 0 is No Car, 1 is Car moving Straight, 2 is Car turning Left
 // 3 is Car turning Right
@@ -11,7 +11,7 @@ const int straight_car = 1;
 const int left_car = 2;
 const int right_car = 3;
 
-// Traffic Light States: 0 is Red Light, 1 is Green Light 
+// Traffic Light States: 0 is Red Light, 1 is Green Light
 const int red = 0;
 const int green = 1;
 
@@ -23,7 +23,7 @@ void startSimulation(int vertical_0[], int vertical_1[], int vertical_2[], int v
 int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[], int vertical_3[], int horizontal_0[], int horizontal_1[], int horizontal_2[], int horizontal_3[], int road_size, bool light_holder[], bool light_state);
 
 int main(int argc, char** argv){
- 
+
   string file_name = argv[1];
   // Total number of cars in one road. Reads the input file and gets array dimensions.
   int road_size = getRoadSize(file_name);
@@ -34,13 +34,15 @@ int main(int argc, char** argv){
   bool west_light = green;
   bool north_light = red;
   bool south_light = red;
-  
+
+  bool light_holder[4] = {west_light, south_light, east_light, north_light};
+
   int horizontal_0 [road_size], horizontal_1 [road_size], horizontal_2 [road_size], horizontal_3 [road_size];
   int vertical_0 [road_size], vertical_1 [road_size], vertical_2 [road_size], vertical_3 [road_size];
 
   total_cars = readInput(file_name, road_size, total_cars, vertical_0, vertical_1, vertical_2, vertical_3, horizontal_0, horizontal_1, horizontal_2, horizontal_3);
 
-  //printRoadTop(vertical_0, vertical_1, vertical_2, vertical_3, road_size);
+  printRoadTop(vertical_0, vertical_1, vertical_2, vertical_3, horizontal_0, horizontal_1, horizontal_2, horizontal_3, road_size, light_holder);
 
   //printRoadData(vertical_0, vertical_1, vertical_2, vertical_3, horizontal_0, horizontal_1, horizontal_2, horizontal_3, road_size);
 
