21fad57738a4174dc05e28647fe36fdeff64096f
Sabyasachi Sahoo <sabyas2009@gmail.com>
2019-12-14 15:22:20 -0600

    OpenMP clear ends completed

--- traffic.cpp
+++ traffic.cpp
@@ -64,12 +64,10 @@ int myRand(int arr[], int freq[], int n)
   prefix[0] = freq[0]; 
   for (i = 1; i < n; ++i) 
     prefix[i] = prefix[i - 1] + freq[i]; 
-
   // prefix[n-1] is sum of all frequencies. 
   // Generate a random number with 
   // value from 1 to this sum 
   int r = (rand() % prefix[n - 1]) + 1; 
-
   // Find index of ceiling of r in prefix arrat 
   int indexc = findCeil(prefix, r, 0, n - 1); 
   return arr[indexc]; 
@@ -87,9 +85,9 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
 
 void clearV0V1Ends(int vertical_0[], int vertical_1[], int road_size, int section_xy);
 
-void clearH0H1Ends(int horizontal_0[], int horizontal_1[], int section_xy);
+void clearH0H1Ends(int horizontal_0[], int horizontal_1[], int section_xy, int road_size);
 
-void clearV2V3Ends(int vertical_2[], int vertical_3[], int section_xy);
+void clearV2V3Ends(int vertical_2[], int vertical_3[], int section_xy, int road_size);
 
 void clearV0V1Fronts(int vertical_0[], int vertical_1[], int horizontal_0[], int intesection_crossing_point);
 
@@ -109,7 +107,6 @@ int main(int argc, char** argv){
   road_size = getRoadSize(file_name);
   std::cout<< "The dimensions are: " << road_size << " \n" ;
   */
-
   int road_size = atoi(argv[1]);
 
   // Lights for each intersection are named in the direction of the cars moving.
@@ -121,7 +118,7 @@ int main(int argc, char** argv){
   // Initialize streets
   int horizontal_0 [road_size], horizontal_1 [road_size], horizontal_2 [road_size], horizontal_3 [road_size];
   int vertical_0 [road_size], vertical_1 [road_size], vertical_2 [road_size], vertical_3 [road_size];
-
+  
   int right_total = makeInputRightTurns(road_size, total_cars, vertical_0, vertical_3, horizontal_0, horizontal_3);
   cout << "Total after right: " << right_total << endl;
   int left_total =  makeInputLeftTurns(road_size, total_cars, vertical_1, vertical_2, horizontal_1, horizontal_2);
@@ -130,6 +127,17 @@ int main(int argc, char** argv){
 
   //total_cars = readInput(file_name, road_size, total_cars, vertical_0, vertical_1, vertical_2, vertical_3, horizontal_0, horizontal_1, horizontal_2, horizontal_3);
   total_cars_2 = total_cars;
+ 
+  /* debuggin openmp next state arr init
+  int section_xy = (road_size - 4)/2;
+  int horizontal_3_next_state[road_size-((road_size-1)-section_xy)];
+  int horizontal_2_next_state[road_size-(section_xy+1)];
+  cout<< "The horizontal 3: " << road_size-((road_size-1)-section_xy) << "\n";
+  cout<< "The horizontal 2: " << road_size-(section_xy+1) << "\n"; 
+  cout<< "The horizontal 3 end index: " << ((road_size-1)-section_xy) << "\n"; 
+  cout<< "The horizontal 2 end index: " << section_xy+1 << "\n"; 
+  cout<< "The horizontal 3 , 2 start index: " << road_size-1 << "\n"; 
+  */
 
   startSimulation(vertical_0, vertical_1, vertical_2, vertical_3, horizontal_0, horizontal_1, horizontal_2, horizontal_3, road_size, east_light, west_light, north_light, south_light);
 
@@ -249,7 +257,7 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
         if( i == section_xy+1 && horizontal_3[i-1] == 2 ){
           horizontal_2[i] = horizontal_2[i-1];
         } else {
-        horizontal_2[i] = horizontal_2[i-1];
+        horizontal_2[i] = horizontal_2[i-1]; //straight road
         }
       }
       for(int i = road_size-1; i>=1; i--){
@@ -258,15 +266,15 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
           vertical_0[((road_size-2)-intesection_crossing_point)] = horizontal_3[i-1];
           horizontal_3[i] = 0;
         } else {
-        horizontal_3[i] = horizontal_3[i-1];
+        horizontal_3[i] = horizontal_3[i-1]; //straight road
         }
       }
       horizontal_2[0] = 0;
       horizontal_3[0] = 0;
       // vertical 2, 3 --- upper part
-      clearV2V3Ends(vertical_2, vertical_3, section_xy);
+      clearV2V3Ends(vertical_2, vertical_3, section_xy, road_size);
       // horizontal 0,1 left part
-      clearH0H1Ends(horizontal_0, horizontal_1, section_xy);
+      clearH0H1Ends(horizontal_0, horizontal_1, section_xy, road_size);
       // vertical 0, 1 --- lower part
       clearV0V1Ends(vertical_0, vertical_1, road_size, section_xy);
       // vertical 0, 1 --- upper part
@@ -312,9 +320,9 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
         }
       }
       // vertical 2, 3 --- upper part
-      clearV2V3Ends(vertical_2, vertical_3, section_xy);
+      clearV2V3Ends(vertical_2, vertical_3, section_xy, road_size);
       // horizontal 0,1 left part
-      clearH0H1Ends(horizontal_0, horizontal_1, section_xy);    
+      clearH0H1Ends(horizontal_0, horizontal_1, section_xy, road_size);    
       // vertical 0, 1
       clearV0V1Ends(vertical_0, vertical_1, road_size, section_xy);
       // vertical 0, 1 --- upper part
@@ -350,7 +358,7 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
       // horizontal 2, 3 left part
       clearH2H3Ends(horizontal_2, horizontal_3, road_size, section_xy);
       // horizontal 0, 1 --- left part
-      clearH0H1Ends(horizontal_0, horizontal_1, section_xy);
+      clearH0H1Ends(horizontal_0, horizontal_1, section_xy, road_size);
       // vertical 0, 1 --- lower part
       clearV0V1Ends(vertical_0, vertical_1, road_size, section_xy);
       // vertical 0, 1 --- upper part
@@ -394,7 +402,7 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
         }
       }
       // horizontal 0, 1 --- left part
-      clearH0H1Ends(horizontal_0, horizontal_1, section_xy);
+      clearH0H1Ends(horizontal_0, horizontal_1, section_xy, road_size);
       // horizontal 2, 3 left part
       clearH2H3Ends(horizontal_2, horizontal_3, road_size, section_xy);
       // vertical 0, 1
@@ -430,7 +438,7 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
       horizontal_0[road_size-1] = 0;
       horizontal_1[road_size-1] = 0;
       // vertical 2, 3 --- upper part
-      clearV2V3Ends(vertical_2, vertical_3, section_xy);
+      clearV2V3Ends(vertical_2, vertical_3, section_xy, road_size);
       // horizontal 2, 3 left part
       clearH2H3Ends(horizontal_2, horizontal_3, road_size, section_xy);
       // vertical 0, 1 --- lower part
@@ -478,7 +486,7 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
       // horizontal 2, 3 left part
       clearH2H3Ends(horizontal_2, horizontal_3, road_size, section_xy);
       // vertical 2, 3 --- upper part
-      clearV2V3Ends(vertical_2, vertical_3, section_xy);
+      clearV2V3Ends(vertical_2, vertical_3, section_xy, road_size);
       // vertical 0, 1
       clearV0V1Ends(vertical_0, vertical_1, road_size, section_xy);
       // vertical 0, 1 --- upper part
@@ -513,8 +521,8 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
       vertical_1[0] = 0;
       clearH2H3Ends(horizontal_2, horizontal_3, road_size, section_xy);
       // vertical 2, 3 --- upper part
-      clearV2V3Ends(vertical_2, vertical_3, section_xy);
-      clearH0H1Ends(horizontal_0, horizontal_1, section_xy);
+      clearV2V3Ends(vertical_2, vertical_3, section_xy, road_size);
+      clearH0H1Ends(horizontal_0, horizontal_1, section_xy, road_size);
       // horizontal 0, 1 --- right part
       clearH0H1Fronts(horizontal_0, horizontal_1, vertical_3, road_size, intesection_crossing_point, section_xy);
       // vertical 2, 3 --- lower part
@@ -556,10 +564,10 @@ int performStateSimulation(int vertical_0[], int vertical_1[], int vertical_2[],
         }
       }
       // vertical 2, 3 --- upper part
-      clearV2V3Ends(vertical_2, vertical_3, section_xy);
+      clearV2V3Ends(vertical_2, vertical_3, section_xy, road_size);
       clearH2H3Ends(horizontal_2, horizontal_3, road_size, section_xy);
       // horizontal 0,1 left part
-      clearH0H1Ends(horizontal_0, horizontal_1, section_xy);
+      clearH0H1Ends(horizontal_0, horizontal_1, section_xy, road_size);
       // horizontal 0, 1 --- right part
       clearH0H1Fronts(horizontal_0, horizontal_1, vertical_3, road_size, intesection_crossing_point, section_xy);
       // vertical 2, 3 --- lower part
@@ -582,45 +590,83 @@ void clearV0V1Ends(int vertical_0[], int vertical_1[], int road_size, int sectio
   if(vertical_1[road_size-1]>0){
       total_cars--;
   }
-  for(int i = road_size-1; i>=((road_size-1)-section_xy); i--){
-    vertical_0[i] = vertical_0[i-1];
+  /* Duplicating arrays  */
+  int vertical_0_next_state[road_size-1];
+  int vertical_1_next_state[road_size-1];
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    vertical_0_next_state[i] = vertical_0[i];
   }
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    vertical_1_next_state[i] = vertical_1[i];
+  }
+  /* Duplicating arrays  */
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=((road_size-1)-section_xy); i--){
+    vertical_0[i] = vertical_0_next_state[i-1];
+  }
+  #pragma omp parallel for
   for(int i = road_size-1; i>=(section_xy+1); i--){
-    vertical_1[i] = vertical_1[i-1];
+    vertical_1[i] = vertical_1_next_state[i-1];
   }
   vertical_1[section_xy+1] = 0;
 }
 
-void clearH0H1Ends(int horizontal_0[], int horizontal_1[], int section_xy){
+void clearH0H1Ends(int horizontal_0[], int horizontal_1[], int section_xy, int road_size){
   if(horizontal_0[0]>0){
       total_cars--;
   }
   if(horizontal_1[0]>0){
       total_cars--;
   }
+  /* Duplicating arrays  */
+  int horizontal_0_next_state[road_size-1];
+  int horizontal_1_next_state[road_size-1];
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    horizontal_0_next_state[i] = horizontal_0[i];
+  }
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    horizontal_1_next_state[i] = horizontal_1[i];
+  }
+  /* Duplicating arrays  */
   for(int i = 0; i<=(section_xy-1); i++){
-    horizontal_0[i] = horizontal_0[i+1];
+    horizontal_0[i] = horizontal_0_next_state[i+1];
   }
   horizontal_0[section_xy] = 0;
   for(int i = 0; i<=(section_xy+2); i++){
-    horizontal_1[i] = horizontal_1[i+1];
+    horizontal_1[i] = horizontal_1_next_state[i+1];
   }
   horizontal_1[section_xy+2] = 0;
 }
 
-void clearV2V3Ends(int vertical_2[], int vertical_3[], int section_xy){
+void clearV2V3Ends(int vertical_2[], int vertical_3[], int section_xy, int road_size){
   if(vertical_2[0]>0){
       total_cars--;
   }
   if(vertical_3[0]>0){
       total_cars--;
   }
+  /* Duplicating arrays  */
+  int vertical_2_next_state[road_size-1];
+  int vertical_3_next_state[road_size-1];
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    vertical_2_next_state[i] = vertical_2[i];
+  }
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    vertical_3_next_state[i] = vertical_3[i];
+  }
+  /* Duplicating arrays  */
   for(int i = 0; i<=(section_xy-1); i++){
-    vertical_3[i] = vertical_3[i+1];
+    vertical_3[i] = vertical_3_next_state[i+1];
   }
   vertical_3[section_xy] = 0;
   for(int i = 0; i<=(section_xy+2); i++){
-    vertical_2[i] = vertical_2[i+1];
+    vertical_2[i] = vertical_2_next_state[i+1];
   }
   vertical_2[section_xy+2] = 0;
 }
@@ -690,11 +736,36 @@ void clearH2H3Ends(int horizontal_2[], int horizontal_3[], int road_size, int se
   if(horizontal_3[road_size-1]>0){
       total_cars--;
   }
-  for(int i = road_size-1; i>=((road_size-1)-section_xy); i--){
-    horizontal_3[i] = horizontal_3[i-1];
+  /* Duplicating arrays  */
+  int horizontal_3_next_state[road_size-1];
+  int horizontal_2_next_state[road_size-1];
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    horizontal_3_next_state[i] = horizontal_3[i];
   }
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=0; i--){
+    horizontal_2_next_state[i] = horizontal_2[i];
+  }
+  /* Duplicating arrays  */
+
+  /* test printing
+  for(int i = road_size-1; i>=0; i--){
+    cout << horizontal_3_next_state[i] << "  ";
+  }
+  cout << endl;
+  for(int i = road_size-1; i>=0; i--){
+    cout << horizontal_3[i] << "  ";
+  }
+  cout << endl;
+  */
+  #pragma omp parallel for
+  for(int i = road_size-1; i>=((road_size-1)-section_xy); i--){
+    horizontal_3[i] = horizontal_3_next_state[i-1];
+  }
+  #pragma omp parallel for
   for(int i = road_size-1; i>=section_xy+1; i--){
-    horizontal_2[i] = horizontal_2[i-1];
+    horizontal_2[i] = horizontal_2_next_state[i-1];
   }
   horizontal_2[section_xy+1] = 0;
 }
@@ -718,11 +789,11 @@ void clearH2H3Fronts(int horizontal_2[], int horizontal_3[], int vertical_0[], i
 }
 
 int makeInputRightTurns(int road_size, int total_cars, int vertical_0[], int vertical_3[], int horizontal_0[], int horizontal_3[]){
-    int size = road_size;
-    int section_xy = (size - 4)/2;
+  int size = road_size;
+  int section_xy = (size - 4)/2;
   int arr[] = {0, 1, 2}; 
   int freq[] = {40, 40, 20}; 
-    int result[size]; 
+  int result[size]; 
   int i, n = sizeof(arr) / sizeof(arr[0]); 
   // Use a different seed value for every run. 
   srand(time(NULL)); 
@@ -756,8 +827,8 @@ int makeInputRightTurns(int road_size, int total_cars, int vertical_0[], int ver
 }
 
 int makeInputLeftTurns(int road_size, int total_cars, int vertical_1[], int vertical_2[], int horizontal_1[], int horizontal_2[]){
-    int size = road_size;
-    int section_xy = (size - 4)/2;
+  int size = road_size;
+  int section_xy = (size - 4)/2;
   int arr[] = {0, 1, 3}; 
   int freq[] = {40, 40, 20}; 
     int result[size]; 
